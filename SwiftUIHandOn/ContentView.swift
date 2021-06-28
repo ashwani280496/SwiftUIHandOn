fbe10c9f95903f81d635bc6f9b48a3ecbbf02234
Ashwani Kumar <ashwani.cse4@gmail.com>
2021-06-28 07:49:08 +0530

    Initial Commit

---
+++ SwiftUIHandOn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIHandOn
+//
+//  Created by Ashwani Shakya on 28/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIHandOn/SwiftUIHandOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHandOnApp.swift
+//  SwiftUIHandOn
+//
+//  Created by Ashwani Shakya on 28/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHandOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
